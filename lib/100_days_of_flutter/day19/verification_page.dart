d64833aaeac34a53b32f45b3faa49a460d7bfeb4
arpitha shetty <shettyarpitha856@gmail.com>
2023-06-17 13:12:39 +0530

    changed theb colour

---
+++ lib/100_days_of_flutter/day19/verification_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class VerificationPage extends StatefulWidget {
+  const VerificationPage({super.key});
+
+  @override
+  State<VerificationPage> createState() => _VerificationPageState();
+}
+class _VerificationPageState extends State<VerificationPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+   
+    );
+  }
+}
